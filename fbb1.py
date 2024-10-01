cf6b65bc1198e6becf1b5dc57047f3f6497006a5
myname <aaa@example.com>
2024-10-01 14:26:06 +0800

    update fbb1.py

--- fbb1.py
+++ fbb1.py
@@ -8,7 +8,7 @@ def fibonacci(n):
         return [0, 1]
     
     fib_series = [0, 1]
-    for i in range(2, n):
+    for _ in range(2, n):
         fib_series.append(fib_series[-1] + fib_series[-2])
     return fib_series
 
