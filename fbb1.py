68a6eb6e4b9f8990540f4a7a7102d75bbe36119d
myname <aaa@example.com>
2024-10-01 14:19:59 +0800

    update fbb1.py

---
+++ fbb1.py
@@ -0,0 +1,24 @@
+
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib_series = [0, 1]
+    for i in range(2, n):
+        fib_series.append(fib_series[-1] + fib_series[-2])
+    return fib_series
+
+def fib(n):    # write Fibonacci series up to n
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=', ')
+        a, b = b, a+b
+    print()
+
+print(fibonacci(7))
+fib(8)
